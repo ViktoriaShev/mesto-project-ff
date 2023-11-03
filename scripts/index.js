14c59ee775936a55e1646a0a0d18404189664216
Viktoria Shev <goo0m@yandex.ru>
2023-11-03 23:33:23 +0300

    theFirst

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,18 @@
-// @todo: Темплейт карточки
+// Обьявление переменных:
+const placesList = document.querySelector(".places__list");
+const cardTemplate = document.querySelector("#card-template").content;
 
-// @todo: DOM узлы
+//Обьявление функций:
+function deleteCard(event) {
+  const selectedCard = event.target;
+  selectedCard.parentElement.remove();
+}
 
-// @todo: Функция создания карточки
-
-// @todo: Функция удаления карточки
-
-// @todo: Вывести карточки на страницу
+initialCards.forEach(function makeCard(initialCard) {
+  const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+  const buttonRemove = cardElement.querySelector(".card__delete-button");
+  cardElement.querySelector(".card__image").src = initialCard.link;
+  cardElement.querySelector(".card__title").textContent = initialCard.name;
+  placesList.append(cardElement);
+  buttonRemove.addEventListener("click", deleteCard);
+});
