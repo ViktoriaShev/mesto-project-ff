6240372a4a6cb57352e7813b25feb5a286cff6ee
Viktoria Shev <goo0m@yandex.ru>
2023-11-04 18:49:15 +0300

    save

--- scripts/index.js
+++ scripts/index.js
@@ -4,15 +4,25 @@ const cardTemplate = document.querySelector("#card-template").content;
 
 //Обьявление функций:
 function deleteCard(event) {
-  const selectedCard = event.target;
-  selectedCard.parentElement.remove();
+  const selectedCard = event.target.closest(".card");
+  selectedCard.remove();
 }
 
-initialCards.forEach(function makeCard(initialCard) {
+function makeCard(cardData, deleteCard) {
   const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
   const buttonRemove = cardElement.querySelector(".card__delete-button");
-  cardElement.querySelector(".card__image").src = initialCard.link;
-  cardElement.querySelector(".card__title").textContent = initialCard.name;
-  placesList.append(cardElement);
+  cardElement.querySelector(".card__image").src = cardData.link;
+  cardElement.querySelector(".card__title").textContent = cardData.name;
+  cardElement.querySelector(".card__image").alt =
+    "Изображение показывает" + cardData.name;
   buttonRemove.addEventListener("click", deleteCard);
-});
+  return cardElement;
+}
+
+function addCard(notReady_card) {
+  const readyCard = makeCard(notReady_card,deleteCard);
+  placesList.append(readyCard);
+}
+
+//Добавление на страницу начальные карточки:
+initialCards.forEach(addCard);
